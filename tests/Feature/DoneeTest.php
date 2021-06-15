fb1a5436dd64027390fbbfe3ecae7a694e336181
Andrew Sears <searsandrew@gmail.com>
2021-06-15 08:32:41 -0500

    Build out Donee tests for views

--- tests/Feature/DoneeTest.php
+++ tests/Feature/DoneeTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Models\Donee;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Route;
 use Tests\TestCase;
 
 class DoneeTest extends TestCase
@@ -21,9 +22,42 @@ class DoneeTest extends TestCase
         $response->assertStatus(200);
     }
 
-    public function test_donee_list_is_generated()
+    public function test_donee_index_can_be_generated()
     {
         $donee = Donee::all();
         $this->assertIsObject($donee);
+    }
+
+    public function test_donee_create_screen_can_be_rendered()
+    {
+        if (! Route::has('donee.create')) {
+            return $this->markTestSkipped('Donee creation not enabled.');
+        }
+
+        $response = $this->get(route('donee.create'));
+
+        $response->assertStatus(200);
+    }
+
+    public function test_donee_show_screen_can_be_rendered()
+    {
+        if (! Route::has('donee.show')) {
+            return $this->markTestSkipped('Donee showing not enabled.');
+        }
+
+        $response = $this->get(route('donee.show'));
+
+        $response->assertStatus(200);
+    }
+
+    public function test_donee_edit_screen_can_be_rendered()
+    {
+        if (! Route::has('donee.edit')) {
+            return $this->markTestSkipped('Donee editing not enabled.');
+        }
+
+        $response = $this->get(route('donee.edit'));
+
+        $response->assertStatus(200);
     }
 }
