f08cd53f2cecd056d7cc441708864d004cb30889
Andrew Sears <searsandrew@gmail.com>
2021-07-02 10:53:56 -0500

    More config options and styles

--- config/shai.php
+++ config/shai.php
@@ -6,7 +6,9 @@ return [
     'mark' => env('APP_MARK', '/images/shai.svg'),
     'card' => env('APP_CARD_LOGO', '/images/shai.svg'),
     'organization' => env('ORGANIZATION_NAME', 'Shai Donation Platform'),
-    'organization_logo' => env('ORGANIZATION_LOGO', '/images/shai.svg'),
+    'organization_logo' => env('ORGANIZATION_LOGO', '/images/shai-logo.svg'),
     'organization_url' => env('ORGANIZATION_URL', 'https://www.github.com/searsandrew/shai'),
-    'organization_description' => env('APP_ORGANIZATION_DESCRIPTION', 'Shai is a donation/giving platform for community churches and organizations that provide seasonal based outreach programs (back-to-school backpacks, Christmas gifts, etc).')
+    'organization_sponsor' => env('ORGANIZATION_SPONSOR', 'https://www.patreon.com/mayfifteenth'),
+    'organization_shop' => env('ORGANIZATION_SHOP', FALSE),
+    'organization_description' => env('ORGANIZATION_DESCRIPTION', 'Shai is a donation/giving platform for community churches and organizations that provide seasonal based outreach programs (back-to-school backpacks, Christmas gifts, etc).')
 ];
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -54,7 +54,7 @@
 
                         <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
                             <div class="flex items-center">
-                                <i class="fas fa-church w-8 h-8 text-gray-500"></i>
+                                <img src="{{ config('shai.organization_logo') }}" style="width: 3rem;" />
                                 <div class="ml-4 text-lg leading-7 font-semibold"><a href="{{ config('shai.organization_url') }}" class="underline text-gray-900 dark:text-white">{{ config('shai.organization') }}</a></div>
                             </div>
 
@@ -70,26 +70,28 @@
                 <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
                     <div class="text-center text-sm text-gray-500 sm:text-left">
                         <div class="flex items-center">
-                            <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="-mt-px w-5 h-5 text-gray-400">
-                                <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
-                            </svg>
+                            @if(config('shai.organization_shop') != FALSE)
+                                <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="-mt-px w-5 h-5 text-gray-400">
+                                    <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
+                                </svg>
 
-                            <a href="https://laravel.bigcartel.com" class="ml-1 underline">
-                                Shop
-                            </a>
+                                <a href="{{ config('shai.organization_shop') }}" class="ml-1 underline">
+                                    Shop
+                                </a>
+                            @endif
 
                             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="ml-4 -mt-px w-5 h-5 text-gray-400">
                                 <path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
                             </svg>
 
-                            <a href="https://github.com/sponsors/taylorotwell" class="ml-1 underline">
-                                Sponsor
+                            <a href="{{ config('shai.organization_donate') }}" class="ml-1 underline">
+                                Donate
                             </a>
                         </div>
                     </div>
 
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
-                        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                        Shai v.{{ config('shai.version') }}
                     </div>
                 </div>
             </div>
