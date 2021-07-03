2e302fa736ac28ebf006e915ea9e6acfe71df093
Andrew Sears <searsandrew@gmail.com>
2021-07-02 19:20:11 -0500

    Set background direct

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
         </style>
     </head>
     <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0" style="background-image: url('{{ $background ?? '' }}');">
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0" style="background-image: url('{{ Storage::url($current->design->background) }}');">
             <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                 @auth
                     <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
