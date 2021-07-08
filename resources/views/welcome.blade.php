69482b673dba3a4778b174252f404f5ee9c2b7a6
Andrew Sears <searsandrew@gmail.com>
2021-07-08 14:31:46 -0500

    Open in new window

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,7 +84,7 @@
                                 <path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
                             </svg>
 
-                            <a href="{{ config('shai.organization_sponsor') }}" class="ml-1 underline">
+                            <a href="{{ config('shai.organization_sponsor') }}" target="_new" class="ml-1 underline">
                                 Donate
                             </a>
                         </div>
