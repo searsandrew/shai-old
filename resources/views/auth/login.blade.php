59872150b00df43cfadcd46e5d10d8b7f6f717cc
Andrew Sears <searsandrew@gmail.com>
2021-11-03 15:17:19 -0500

    Add register link to login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -44,5 +44,9 @@
                 </x-jet-button>
             </div>
         </form>
+        <hr class="my-6"/>
+        <a class="inline-flex flex-grow items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition" href="{{ route('register') }}">
+            Click Here to Register
+        </a>
     </x-jet-authentication-card>
 </x-guest-layout>
