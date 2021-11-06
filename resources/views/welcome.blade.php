6ea493aaaab65b5f9ff9cdbf66d85d3a818713a6
Andrew Sears <searsandrew@gmail.com>
2021-11-06 07:32:34 -0500

    img fluid

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0" style="background-image: url('{{ Storage::url($current->background) }}');">
-            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+            <div class="block fixed top-0 right-0 px-6 py-4">
                 @auth
                     <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
                 @else
@@ -34,7 +34,7 @@
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                    <img src="{{ Storage::url($current->logo) }}" />
+                    <img class="img-fluid" src="{{ Storage::url($current->logo) }}" />
                 </div>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
