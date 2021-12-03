85ddfbd4512242cd1f897f0b33b763c3310d37d7
Andrew Sears <searsandrew@gmail.com>
2021-12-03 11:38:06 -0600

    Add contructor and other methods

--- app/Mail/DonorReminder.php
+++ app/Mail/DonorReminder.php
@@ -2,6 +2,9 @@
 
 namespace App\Mail;
 
+use App\Models\Campaign;
+use App\Models\User;
+use App\Models\Wishlist;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
@@ -11,14 +14,29 @@ class DonorReminder extends Mailable
 {
     use Queueable, SerializesModels;
 
+    public Campaign $campaign;
+    public $wishlists;
+    public User $user;
+
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Wishlist $wishlist)
     {
-        //
+        $this->campaign = $wishlist->campaign;
+        $this->user = $wishlist->user;
+        $this->wishlists = $this->getWishlists();
+    }
+
+    public function getWishlists()
+    {
+        if($this->campaign->family == 'true')
+        {
+            return $this->user->familyWishlistsByCampaign($this->campaign->id);
+        }
+        return $this->user->wishlistsByCampaign($this->campaign->id);
     }
 
     /**
